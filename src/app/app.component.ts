eba0c72bea93438e3c5fa978447a1e560fef2f2b
jasonbirra <jose.celisv@hotmail.com>
2023-03-12 00:03:32 -0300

    inicializando splash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+import {Platform} from '@ionic/angular';
+import { StatusBar } from '@awesome-cordova-plugins/status-bar/ngx';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
 export class AppComponent {
-  constructor() {}
+  constructor(
+    //private statusBar: StatusBar,
+    private platform: Platform,   
+    public router:Router
+
+  ) {
+    this.initializeApp();
+  }
+
+
+  initializeApp(){
+      this.platform.ready().then(()=> {
+      //this.statusBar.styleDefault();      
+      this.router.navigateByUrl('splash')
+    });
+
+  }
 }
